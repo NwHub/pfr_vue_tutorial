17bae93f55f3fba483bd5d5e8887ab5b9cd8e17f
zawa <shiro83@gmail.com>
2019-05-12 04:58:45 +0900

    lesson3

---
+++ Lesson_03/prf_vue_lesson/components/ui/CounterContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <CounterButton />
+    <CounterView />
+  </div>
+</template>
+
+<script>
+import CounterButton from "~/components/ui/CounterButton.vue";
+import CounterView from "~/components/ui/CounterView.vue";
+
+export default {
+  components: {
+    CounterButton,
+    CounterView
+  }
+}
+</script>
---
+++ Lesson_03/prf_vue_lesson/pages/todo.vue
@@ -0,0 +1,48 @@
+<template>
+  <dev>
+    <p class="text-xs-center display-3 font-weight-medium">
+      Todo
+    </p>
+
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+
+          <v-subheader>
+            Your Life
+          </v-subheader>
+
+          <v-list two-line>
+            <dev v-for="(todoData, index) in todoDatas">
+
+              <v-divider></v-divider>
+
+              <v-list-tile>
+                <v-list-tile-avatar>
+                  {{ index }}
+                </v-list-tile-avatar>
+
+                <v-list-tile-content>
+                  <v-list-tile-title><p class="title">{{ todoData.content }}</p></v-list-tile-title>
+                  <v-list-tile-sub-title>{{ todoData.created }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+
+            </dev>
+
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </dev>
+</template>
+
+<script>
+export default {
+  computed: {
+    todoDatas () {
+      return this.$store.state.todo.list
+    }
+  }
+}
+</script>
