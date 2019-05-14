9e36af819307ab2231200808bec14f4a369fbb26
zawa <shiro83@gmail.com>
2019-05-15 07:30:12 +0900

    lesson5

---
+++ Lesson_05/prf_vue_lesson/pages/page1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p class="text-xs-center display-3 font-weight-medium">Page1</p>
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from "~/components/ui/CounterContainer.vue";
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
